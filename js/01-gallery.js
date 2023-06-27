c5d8be5fcc13033fde23f0828c79224681514356
Wojciech Maroszek <122974346+WojciechMaroszek1@users.noreply.github.com>
2023-06-27 13:53:21 +0200

    fix: lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -12,56 +12,19 @@ const galleryImages = galleryItems
 gallery.insertAdjacentHTML("beforeend", galleryImages);
 console.log(gallery);
 
-// gallery.addEventListener("click", element => {
-//     element.preventDefault();
-//     if (element.target.nodeName !== "IMG") return;
-//     const imageDataSource = element.target.getAttribute("data-source");
-//     const instance = basicLightbox.create(
-//         `<img src="${imageDataSource}" width="800" height="600">`,
-//         {
-//             onShow: (instance) => {
-//                 document.addEventListener("keydown", (element) => {
-//                     if (element.key === "Escape") instance.close();
-//                 });
-//             }
-//         });
-//     instance.show();
-// });
-
-// gallery.addEventListener("click", e => {
-//     e.preventDefault();
-//     if (e.target.nodeName !== "IMG") return;
-//     const handleEscapeKey = e => { if (e.key === "Escape") instance.close() };
-// const instance = basicLightbox.create(
-//         `<img src="${e.target.dataset.source}">`,
-//         {
-//             onShow: () => {
-//                 document.addEventListener("keydown", (e) => {
-//                     if (e.key === "Escape") instance.close();
-//                 });
-//             },
-//             onClose: () => {
-//                 document.removeEventListener("keydown", (e) => {
-//                     if (e.key === "Escape") instance.close();
-//                 });
-//             },
-//         });
-//     instance.show();
-// });
-
-// gallery.addEventListener("click", element => {
-//     element.preventDefault();
-//     if (element.target.nodeName !== "IMG") return;
-//     const handleEscapeKey = element => { if (element.key === "Escape") instance.close() };
-//     const instance = basicLightbox.create(
-//         `<img src="${element.target.dataset.source}">`,
-//         {
-//             onShow: () => {
-//                 document.addEventListener("keydown", handleEscapeKey);
-//             },
-//             onClose: () => {
-//                 document.removeEventListener("keydown", handleEscapeKey);
-//             },
-//         });
-//     instance.show();
-// });
+gallery.addEventListener("click", element => {
+    element.preventDefault();
+    if (element.target.nodeName !== "IMG") return;
+    const handleEscapeKey = element => { if (element.key === "Escape") instance.close() };
+    const instance = basicLightbox.create(
+        `<img src="${element.target.dataset.source}">`,
+        {
+            onShow: () => {
+                document.addEventListener("keydown", handleEscapeKey);
+            },
+            onClose: () => {
+                document.removeEventListener("keydown", handleEscapeKey);
+            },
+        });
+    instance.show();
+});
