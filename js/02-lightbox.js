600f55a8fe59e4d11f7d7e1de54a9761ea43740f
Wojciech Maroszek <122974346+WojciechMaroszek1@users.noreply.github.com>
2023-06-27 16:21:50 +0200

    feat: task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector("ul.gallery");
+const galleryImages = galleryItems
+    .map(image =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+            </a>
+        </li>`)
+    .join("");
+gallery.insertAdjacentHTML("beforeend", galleryImages);
+console.log(gallery);
 
-console.log(galleryItems);
+gallery.addEventListener("click", element => {
+    element.preventDefault();
+    if (element.target.nodeName !== "IMG") return; }); 
+
+
+let lightboxGallery = new SimpleLightbox('.gallery a', {captionsData: `alt`, captionDelay: 250});
+
+
+
